23b4e967fe527bb86709c262359966320691cc5a
sisskind <josh.sisskind@digitalglobe.com>
2017-02-07 21:43:46 -0500

    Create buttons to import data

--- modules/ui/layer_menu.js
+++ modules/ui/layer_menu.js
@@ -9,7 +9,44 @@ import { behaviorHash } from '../behavior/index';
 export function uiLayerMenu(context) {
 
     return function(selection) {
+        var _form = null;
+
+        var data = [
+            {dataset:'primary',text:'Add Reference Dataset'},
+            {dataset:'secondary', text: 'Add Secondary Dataset'}
+        ];
+
         var div = selection
+            .append('div')
+            .attr('class','notice');
+
+        div.selectAll('div')
+            .data(data)
+            .enter()
+            .append('form')
+            .classed('round space-bottom1 strong', true)
+            .on('submit',function(){
+                d3.event.stopPropagation();
+                d3.event.preventDefault();
+            })
+            .append('button')
+            .classed('col4', true)
+            .text(function(d){
+                return d.text;
+            })
+            .on('click',function(){
+                d3.event.stopPropagation();
+                d3.event.preventDefault();
+            })
+            .append('span')
+            .text(function(){return data.text;});
+
+
+
+
+
+
+       /* var div = selection
             .append('div')
             .attr('class', 'notice');
 
@@ -17,15 +54,14 @@ export function uiLayerMenu(context) {
             .append('button')
             .attr('class', 'zoom-to notice')
             .on('click', function() { 
-                var key = {name: "AllDataTypesA", id: "699", color: "violet"};
-                preAddLayer(key);
+                
             });
 
         button
             .call(svgIcon('#icon-plus', 'pre-text'))
             .append('span')
             .attr('class', 'label')
-            .text('Add Layer');
+            .text('Add Layer');*/
 
         function disableTooHigh() {
             div.style('display', context.editable() ? 'none' : 'block');
